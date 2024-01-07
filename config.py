f9785ab1875e78413537fe67c5a7475ca260910a
socialrating <mdk_bol@bk.ru>
2024-01-07 21:23:16 +0500

    Добавил создание и удаление каналов

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+#ID категории: ID канала для создания комнат
+custom_voice = {
+    1193564053541486655: 1193588535450677468
+}
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
---
+++ main.py
@@ -0,0 +1,46 @@
+# This example requires the 'message_content' intent.
+import discord
+from discord.ext import tasks
+
+import config
+
+created_channels = set()
+
+
+class MyClient(discord.Client):
+    @tasks.loop(minutes=1)
+    async def check_channels(self):
+        for guild in self.guilds:
+            for category_id in config.custom_voice.keys():
+                category = discord.utils.get(guild.categories, id=category_id)
+                if not category:
+                    return
+                for channel in category.voice_channels:
+                    if channel.id != config.custom_voice[category_id] and len(channel.members) == 0:
+                        await channel.delete()
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        self.check_channels.start()
+
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState,
+                                    after: discord.VoiceState):
+        created_room = None
+        if after.channel is not None and after.channel.category is not None:
+            created_room = config.custom_voice.get(after.channel.category.id)
+        if after.channel is not None and created_room == after.channel.id:
+            overwrites = {member: discord.PermissionOverwrite(move_members=True, mute_members=True, kick_members=True)}
+            new_channel = await after.channel.category.create_voice_channel(member.name, overwrites=overwrites)
+            await member.move_to(new_channel)
+            created_channels.add(new_channel.id)
+        if before.channel is not None and before.channel.id in created_channels:
+            if len(before.channel.members) == 0:
+                await before.channel.delete()
+                created_channels.remove(before.channel.id)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+client = MyClient(intents=intents)
+client.run(config.TOKEN)
